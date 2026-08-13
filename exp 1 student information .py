bb8e81971af90fea7cf9af68950770a8b28a794f
srushtibhingardeve1-sketch <srushtibhingardeve1@gmail.com>
2026-08-13 16:24:15 +0530

    Add files via upload

---
+++ exp 1 student information .py
@@ -0,0 +1,8 @@
+print("------STUDENT INFORMATION------")
+
+name=(input("enter student name :-"))
+age=int(input("enter your age:-"))
+college name=input("enter college name:-")
+course name=input("enter your course name:-")
+prn=int(input("enter your prn:-"))
+
