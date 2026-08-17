c1092cbce4880019a81a58d1893417d666103628
srushtibhingardeve1-sketch <srushtibhingardeve1@gmail.com>
2026-08-17 13:13:22 +0530

    Delete exp 1 student information .py

--- exp 1 student information .py
+++
@@ -1,8 +0,0 @@
-print("------STUDENT INFORMATION------")
-
-name=(input("enter student name :-"))
-age=int(input("enter your age:-"))
-college name=input("enter college name:-")
-course name=input("enter your course name:-")
-prn=int(input("enter your prn:-"))
-
