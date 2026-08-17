b2b7d6f39b4419d6a6469818813e9e614e3f459b
srushtibhingardeve1-sketch <srushtibhingardeve1@gmail.com>
2026-08-17 13:14:09 +0530

    Add files via upload

---
+++ prac 1.py
@@ -0,0 +1,24 @@
+print("===================================")
+print("     STUDENT INFORMATION SYSTEM")
+print("===================================")
+
+name = input("Enter student name: ")
+roll_no = input("Enter roll number: ")
+age = input("Enter age: ")
+course = input("Enter course: ")
+college = input("Enter college name: ")
+email = input("Enter email: ")
+
+
+print("\n===================================")
+print("       STUDENT INFORMATION")
+print("===================================")
+
+print("Name      :", name)
+print("Roll No   :", roll_no)
+print("Age       :", age)
+print("Course    :", course)
+print("College   :", college)
+print("Email     :", email)
+
+print("===================================")
